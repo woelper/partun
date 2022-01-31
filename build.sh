ef02688e443eb958c1c3a0fc926798ae52ce17e3
Johann Woelper <woelper@gmail.com>
2022-01-31 21:17:14 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
+# Note: This needs docker
+
+cargo install --force cargo-strip
+cargo install cross
 # ARM6
 cross build --target arm-unknown-linux-gnueabihf  --release
+cargo strip --target arm-unknown-linux-gnueabihf
 # ARM7
 cross build --target armv7-unknown-linux-gnueabihf  --release
+cargo strip --target armv7-unknown-linux-gnueabihf
 
 cp target/arm-unknown-linux-gnueabihf/release/partun release/arm6/
 cp target/armv7-unknown-linux-gnueabihf/release/partun release/arm7/
-git add release
-git commit -m "Cross compile release"
+#git add release
+#git commit -m "Cross compile release"
